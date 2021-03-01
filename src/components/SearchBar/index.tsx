a60c5344f7a85b8166dbe558d0c143e148513cae
Sam Kim <sam@workclout.com>
2021-02-28 16:02:09 -0800

    add onChange event to input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { InputGroup, Input, InputLeftElement } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
-const SearchBar = () => {
+interface SearchBarProps {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onChange }) => {
 
   return (
     <InputGroup>
       <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
-      <Input type="text" placeholder="Search this user's repositories..." />
+      <Input type="text" placeholder="Search this user's repositories..." onChange={onChange} />
     </InputGroup>
   )
 }
--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import SearchBar from '../../components/SearchBar';
 
 const Home: React.FC = () => {
+  const [user, setUser] = useState<string>('');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setUser(event.target.value);
+
   return (
     <Box height="100vh">
       <Flex height="100%" alignItems="center" margin="0 auto" w={[200, 400, 600]}>
-        <SearchBar />
+        <SearchBar onChange={handleChange} />
       </Flex>
     </Box>
   );
